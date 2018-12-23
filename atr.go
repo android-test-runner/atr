5d2b61659d5e74530999221666cb968e09b7e8bf
Yves Bonjour <yves.bonjour@ergon.ch>
2018-12-23 13:17:37 +0100

    Extracts test command to test.go

--- atr.go
+++ atr.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli"
 	"os"
 )
 
 var version string //needs to be set when compiling using ldflags (-ldflags "-X main.version=42")
-
-var testCommand = cli.Command{
-	Name:   "test",
-	Usage:  "Execute an android instrumentation test",
-	Action: test,
-	Flags: []cli.Flag{
-		cli.StringFlag{
-			Name:  "apk, a",
-			Usage: "APK under test",
-		},
-		cli.StringFlag{
-			Name:  "testapk, ta",
-			Usage: "APK containing instrumentation tests",
-		},
-		cli.StringSliceFlag{
-			Name:  "test, t",
-			Usage: "Test to execute",
-		},
-	},
-}
 
 func main() {
 	app := cli.NewApp()
@@ -39,11 +18,4 @@ func main() {
 	}
 
 	app.Run(os.Args)
-}
-
-func test(c *cli.Context) error {
-	fmt.Printf("%v\n", c.String("apk"))
-	fmt.Printf("%v\n", c.String("testapk"))
-	fmt.Printf("%v\n", c.String("test"))
-	return nil
 }
---
+++ test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+)
+
+var testCommand = cli.Command{
+	Name:   "test",
+	Usage:  "Execute an android instrumentation test",
+	Action: test,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "apk, a",
+			Usage: "APK under test",
+		},
+		cli.StringFlag{
+			Name:  "testapk, ta",
+			Usage: "APK containing instrumentation tests",
+		},
+		cli.StringSliceFlag{
+			Name:  "test, t",
+			Usage: "Test to execute",
+		},
+	},
+}
+
+func test(c *cli.Context) error {
+	fmt.Printf("%v\n", c.String("apk"))
+	fmt.Printf("%v\n", c.String("testapk"))
+	fmt.Printf("%v\n", c.String("test"))
+	return nil
+}
