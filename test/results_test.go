6c49a27a93aeb9a5b767386ebb25ecb701a5de57
Yves Bonjour <yves.bonjour@ergon.ch>
2018-12-28 09:42:55 +0100

    Adds tests for TestResult parsing

---
+++ test/results_test.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestResultFromOutputPassed(t *testing.T) {
+	test := Test{Class: "TestClass", Method: "testMethod"}
+	okOutput := "OK (1 test)"
+
+	result := ResultFromOutput(test, nil, okOutput)
+
+	expected := TestResult{Test: test, HasPassed: true, Output: okOutput}
+	if expected != result {
+		t.Error(fmt.Sprintf("Test result is %v instead of %v", result, expected))
+	}
+}
+
+func TestResultFromOutputPassedWithMultilineOutput(t *testing.T) {
+	test := Test{Class: "TestClass", Method: "testMethod"}
+	okOutput := "Some Information\n...\nOK (1 test)"
+
+	result := ResultFromOutput(test, nil, okOutput)
+
+	expected := TestResult{Test: test, HasPassed: true, Output: okOutput}
+	if expected != result {
+		t.Error(fmt.Sprintf("Test result is %v instead of %v", result, expected))
+	}
+}
+
+func TestResultFromOutputWithError(t *testing.T) {
+	test := Test{Class: "TestClass", Method: "testMethod"}
+	err := errors.New("some error")
+	okOutput := "OK (1 test)"
+
+	result := ResultFromOutput(test, err, okOutput)
+
+	expected := TestResult{Test: test, HasPassed: false, Output: okOutput}
+	if expected != result {
+		t.Error(fmt.Sprintf("Test result is %v instead of %v", result, expected))
+	}
+}
+
+func TestResultFromOutputWithFailureOutput(t *testing.T) {
+	test := Test{Class: "TestClass", Method: "testMethod"}
+	failureOutput := "Failure"
+
+	result := ResultFromOutput(test, nil, failureOutput)
+
+	expected := TestResult{Test: test, HasPassed: false, Output: failureOutput}
+	if expected != result {
+		t.Error(fmt.Sprintf("Test result is %v instead of %v", result, expected))
+	}
+}
