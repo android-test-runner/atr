2e800d0e49de90656a241f03547161a523847ec8
Yves Bonjour <yves.bonjour@ergon.ch>
2018-12-28 09:46:17 +0100

    Extracts output_parsing from aapt

--- aapt/aapt.go
+++ aapt/aapt.go
@@ -1,11 +1,8 @@
 package aapt
 
 import (
-	"errors"
 	"github.com/ybonjour/atr/command"
 	"os/exec"
-	"regexp"
-	"strings"
 )
 
 func PackageName(apkPath string) (string, error) {
@@ -14,16 +11,7 @@ func PackageName(apkPath string) (string, error) {
 		return "", err
 	}
 
-	lines := strings.Split(out, "\n")
-	for _, line := range lines {
-		r := regexp.MustCompile(`package: name='([^']+)'`)
-		matches := r.FindStringSubmatch(line)
-		if matches != nil {
-			return matches[1], nil
-		}
-	}
-
-	return "", errors.New("package name not found")
+	return ParsePackageName(out)
 }
 
 func TestRunner(apkPath string) (string, error) {
@@ -39,25 +27,5 @@ func TestRunner(apkPath string) (string, error) {
 		return "", err
 	}
 
-	lines := strings.Split(out, "\n")
-	startInstrumentationIdx := len(lines)
-	for idx := 0; idx < len(lines); idx++ {
-		line := lines[idx]
-		regexInstrumentation := regexp.MustCompile(`E: instrumentation`)
-		if regexInstrumentation.FindStringSubmatch(line) != nil {
-			startInstrumentationIdx = idx
-			break
-		}
-	}
-
-	for idx := startInstrumentationIdx + 1; idx < len(lines); idx++ {
-		line := lines[idx]
-		regexName := regexp.MustCompile(`A: android:name\([^)]+\)="([^"]+)"`)
-		matches := regexName.FindStringSubmatch(line)
-		if matches != nil {
-			return matches[1], nil
-		}
-	}
-
-	return "", errors.New("No test runner found in AndroidManifest")
+	return ParseTestRunner(out)
 }
---
+++ aapt/output_parsing.go
@@ -0,0 +1,44 @@
+package aapt
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+func ParsePackageName(out string) (string, error) {
+	lines := strings.Split(out, "\n")
+	for _, line := range lines {
+		r := regexp.MustCompile(`package: name='([^']+)'`)
+		matches := r.FindStringSubmatch(line)
+		if matches != nil {
+			return matches[1], nil
+		}
+	}
+
+	return "", errors.New("package name not found")
+}
+
+func ParseTestRunner(out string) (string, error) {
+	lines := strings.Split(out, "\n")
+	startInstrumentationIdx := len(lines)
+	for idx := 0; idx < len(lines); idx++ {
+		line := lines[idx]
+		regexInstrumentation := regexp.MustCompile(`E: instrumentation`)
+		if regexInstrumentation.FindStringSubmatch(line) != nil {
+			startInstrumentationIdx = idx
+			break
+		}
+	}
+
+	for idx := startInstrumentationIdx + 1; idx < len(lines); idx++ {
+		line := lines[idx]
+		regexName := regexp.MustCompile(`A: android:name\([^)]+\)="([^"]+)"`)
+		matches := regexName.FindStringSubmatch(line)
+		if matches != nil {
+			return matches[1], nil
+		}
+	}
+
+	return "", errors.New("No test runner found in AndroidManifest")
+}
