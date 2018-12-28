726b80e4d05cc755da791b9a068d0c877d0efad6
Yves Bonjour <yves.bonjour@ergon.ch>
2018-12-28 17:12:29 +0100

    Removes unused import

--- test/parser.go
+++ test/parser.go
@@ -1,19 +1,9 @@
 package test
 
 import (
-	"fmt"
 	"github.com/ybonjour/atr/files"
 	"strings"
 )
-
-type Test struct {
-	Class  string
-	Method string
-}
-
-func (t Test) FullName() string {
-	return fmt.Sprintf("%v#%v", t.Class, t.Method)
-}
 
 type Parser interface {
 	ParseFromFile(path string) ([]Test, error)
