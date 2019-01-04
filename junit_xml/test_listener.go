c97691b5d206fedaa7954a93ec48581d0d3bd520
Yves Bonjour <yves.bonjour@ergon.ch>
2019-01-04 13:32:55 +0100

    #1 Adds junit_xml.TestListener

---
+++ junit_xml/test_listener.go
@@ -0,0 +1,50 @@
+package junit_xml
+
+import (
+	"fmt"
+	"github.com/ybonjour/atr/apks"
+	"github.com/ybonjour/atr/devices"
+	"github.com/ybonjour/atr/output"
+	"github.com/ybonjour/atr/result"
+	"github.com/ybonjour/atr/test"
+	"github.com/ybonjour/atr/test_listener"
+)
+
+type testListener struct {
+	formatter Formatter
+	writer    output.Writer
+	apk       apks.Apk
+	results   []result.Result
+	device    devices.Device
+}
+
+func NewTestListener(writer output.Writer, apk apks.Apk) test_listener.TestListener {
+	return &testListener{
+		formatter: NewFormatter(),
+		apk:       apk,
+		writer:    writer,
+	}
+}
+
+func (listener *testListener) BeforeTestSuite(device devices.Device) {
+	listener.results = []result.Result{}
+	listener.device = device
+}
+
+func (listener *testListener) BeforeTest(test test.Test) {}
+
+func (listener *testListener) AfterTest(result result.Result) {
+	listener.results = append(listener.results, result)
+}
+
+func (listener *testListener) AfterTestSuite() {
+	file, errFormat := listener.formatter.Format(listener.results, listener.apk)
+	if errFormat != nil {
+		fmt.Printf("Could not format xml junit results for device '%v': '%v'", listener.device, errFormat)
+		return
+	}
+	errWrite := listener.writer.WriteFile(file, listener.device)
+	if errWrite != nil {
+		fmt.Printf("Could not write xml junit report '%v'", errWrite)
+	}
+}
