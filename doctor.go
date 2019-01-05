713d1a08723784aa47d39784b9f0e890866e2fc4
Yves Bonjour <yves.bonjour@ergon.ch>
2019-01-05 18:18:16 +0100

    #2 Adds some ascii art

--- doctor.go
+++ doctor.go
@@ -15,13 +15,30 @@ var doctorCommand = cli.Command{
 	Action: doctorAction,
 }
 
+type check func() bool
+
 func doctorAction(c *cli.Context) {
-	fmt.Printf("Results:\n")
-	checkAdbResult := checkAdb()
-	checkAapt()
-	if checkAdbResult {
-		checkConnectedDevices()
+	fmt.Printf("Hi, I am the atr doctor and I am going to do some examinations:\n%v\n", console.AsciiDoctor())
+	checks := []check{
+		checkAdb,
+		checkAapt,
+		checkConnectedDevices,
 	}
+	passed := executeChecks(checks)
+	if passed {
+		fmt.Printf(console.Color("\nAll good. Have a nice day!\n", console.Green))
+	} else {
+		fmt.Printf(console.Color("\nI detected some problems that need to be fixed before you can run tests with atr.\n", console.Red))
+	}
+}
+
+func executeChecks(checks []check) bool {
+	overallResult := true
+	for _, c := range checks {
+		result := c()
+		overallResult = overallResult && result
+	}
+	return overallResult
 }
 
 func checkAdb() bool {
@@ -37,28 +54,33 @@ func checkAdb() bool {
 	}
 }
 
-func checkConnectedDevices() {
+func checkConnectedDevices() bool {
 	connectedDevices, err := adb.New().ConnectedDevices()
 	if err != nil {
 		printError("unable to get connected devices", err.Error())
+		return false
 	} else if len(connectedDevices) == 0 {
 		printError("no connected devices",
 			"- Make sure a device is plugged in or an emluator is started.\n"+
 				"- Make sure the devices are reachable through adb: \n"+
 				"  Execute 'adb devices' and check if the devices are listed with status `device`.")
+		return false
 	} else {
 		printOk(fmt.Sprintf("%v connected devices", len(connectedDevices)))
+		return true
 	}
 }
 
-func checkAapt() {
+func checkAapt() bool {
 	aaptVersion, err := aapt.New().Version()
 	if err != nil {
 		printError("aapt is not installed or not in PATH",
 			"- Install the Android Asset Packaging Tool (aapt). See https://developer.android.com/studio/command-line/aapt2 \n"+
 				"- Make sure aapt can be executed from the command line. You might need to add the path to aapt to the PATH environment variable")
+		return false
 	} else {
 		printOk(fmt.Sprintf("aapt version %v installed", aaptVersion))
+		return true
 	}
 }
 
@@ -73,11 +95,13 @@ func printError(message string, details string) {
 }
 
 func printResult(indicator string, message string, details string) {
-	fmt.Printf("\t%v %v\n", indicator, message)
+	indentSmall := "  "
+	fmt.Printf("%v%v %v\n", indentSmall, indicator, message)
 	if details == "" {
 		return
 	}
 	// Ensure indentation is correct
-	details = strings.Replace(details, "\n", "\n\t  ", -1)
-	fmt.Printf("\t  %v\n", details)
+	indentBig := "    "
+	details = strings.Replace(details, "\n", fmt.Sprintf("\n%v", indentBig), -1)
+	fmt.Printf("%v%v\n", indentBig, details)
 }
