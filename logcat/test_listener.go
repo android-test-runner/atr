28b2de67b91439c91be5a23fc31afce8dcb132ec
Yves Bonjour <yves.bonjour@ergon.ch>
2019-01-12 18:41:42 +0100

    #3 Uses loggger in logcat.test_listener

--- logcat/test_listener.go
+++ logcat/test_listener.go
@@ -1,8 +1,8 @@
 package logcat
 
 import (
-	"fmt"
 	"github.com/ybonjour/atr/devices"
+	"github.com/ybonjour/atr/logging"
 	"github.com/ybonjour/atr/output"
 	"github.com/ybonjour/atr/result"
 	"github.com/ybonjour/atr/test"
@@ -13,6 +13,7 @@ type testListener struct {
 	device devices.Device
 	writer output.Writer
 	logcat Logcat
+	logger logging.Logger
 }
 
 func NewTestListener(device devices.Device, writer output.Writer) test_listener.TestListener {
@@ -20,6 +21,7 @@ func NewTestListener(device devices.Device, writer output.Writer) test_listener.
 		device: device,
 		writer: writer,
 		logcat: New(device),
+		logger: logging.NewForDevice(device),
 	}
 }
 
@@ -30,21 +32,21 @@ func (listener *testListener) AfterTestSuite() {}
 func (listener *testListener) BeforeTest(test test.Test) {
 	errStartLogcat := listener.logcat.StartRecording(test)
 	if errStartLogcat != nil {
-		fmt.Printf("Could not clear logcat: '%v'\n", errStartLogcat)
+		listener.logger.Error("Could not clear logcat", errStartLogcat)
 	}
 }
 
 func (listener *testListener) AfterTest(r result.Result) []result.Extra {
 	errStopLogcat := listener.logcat.StopRecording(r.Test)
 	if errStopLogcat != nil {
-		fmt.Printf("Could not get logcat: '%v'\n", errStopLogcat)
+		listener.logger.Error("Coud not get logcat", errStopLogcat)
 	}
 
 	extras := []result.Extra{}
 	if r.IsFailure() {
 		pathToFile, errSave := listener.logcat.SaveRecording(r.Test, listener.writer)
 		if errSave != nil {
-			fmt.Printf("Could not save logcat: '%v'\n", errSave)
+			listener.logger.Error("Could not save logcat", errSave)
 		} else {
 			extras = append(extras, result.Extra{Name: "Logcat", Value: pathToFile, Type: result.File})
 		}
